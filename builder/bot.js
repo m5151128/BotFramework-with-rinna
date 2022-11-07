f91b5448a0abddc6a0d41d4012d3dd8e13c81dd1
m5151128 <nagayama@thedott.io>
2022-11-07 15:09:31 +0900

    ecceの結果を返すように

--- builder/bot.js
+++ builder/bot.js
@@ -2,16 +2,38 @@
 // Licensed under the MIT License.
 
 const { ActivityHandler, MessageFactory } = require('botbuilder');
+const fetch = require('node-fetch');
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
+            await fetch('https://api.rinna.co.jp/models/ecce', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Cache-Control': 'no-cache',
+                    'Ocp-Apim-Subscription-Key': process.env.RINNA_SUBSCRIPTIONKEY_ECCE
+                },
+                body: JSON.stringify({
+                    'knowledgePath': 'ECCE_Sample.txt',
+                    'query': context.activity.text,
+                    'dialogHistory': [],
+                    'l2ReturnNum': 3,
+                    'l3ReturnNum': 1
+                })
+            })
+                .then(response => response.text())
+                .then(async (result) => {
+                    console.log(result);
+                    const data = JSON.parse(result);
+                    const replyText = data.resultResponseText;
+                    await context.sendActivity(MessageFactory.text(replyText, replyText));
+                    // By calling next() you ensure that the next BotHandler is run.
+                    await next();
+                })
+                .catch(error => console.log('error', error));
         });
 
         this.onMembersAdded(async (context, next) => {
