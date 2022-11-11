7922a7ac281bd4aebd59358398297b523f56cf36
m5151128 <nagayama@thedott.io>
2022-11-11 10:11:21 +0900

    format修正

--- builder/index.ts
+++ builder/index.ts
@@ -1,37 +1,41 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const path = require('path');
+const path = require("path");
 
 // Read environment variables from .env file
-const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+const ENV_FILE = path.join(__dirname, ".env");
+require("dotenv").config({ path: ENV_FILE });
 
-const restify = require('restify');
+const restify = require("restify");
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const {
-    CloudAdapter,
-    ConversationState,
-    MemoryStorage,
-    ConfigurationBotFrameworkAuthentication
-} = require('botbuilder');
+  CloudAdapter,
+  ConversationState,
+  MemoryStorage,
+  ConfigurationBotFrameworkAuthentication,
+} = require("botbuilder");
 
 // This bot's main dialog.
-const { RinnaBot } = require('./bot');
+const { RinnaBot } = require("./bot");
 
 // Create HTTP server
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log(`\n${server.name} listening to ${server.url}`);
+  console.log(
+    "\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator"
+  );
+  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+  process.env
+);
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
@@ -49,29 +53,31 @@ const bot = new RinnaBot(conversationState);
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry
-    //       configuration instructions.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+  // This check writes out errors to console log .vs. app insights.
+  // NOTE: In production environment, you should consider logging this to Azure
+  //       application insights. See https://aka.ms/bottelemetry for telemetry
+  //       configuration instructions.
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+  // Send a trace activity, which will be displayed in Bot Framework Emulator
+  await context.sendTraceActivity(
+    "OnTurnError Trace",
+    `${error}`,
+    "https://www.botframework.com/schemas/error",
+    "TurnError"
+  );
 
-    // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
-    // Clear out state
-    await conversationState.delete(context);
+  // Send a message to the user
+  await context.sendActivity("The bot encountered an error or bug.");
+  await context.sendActivity(
+    "To continue to run this bot, please fix the bot source code."
+  );
+  // Clear out state
+  await conversationState.delete(context);
 };
 
 // Listen for incoming requests.
-server.post('/api/messages', async (req, res) => {
-    // Route received a request to adapter for processing
-    await adapter.process(req, res, (context) => bot.run(context));
+server.post("/api/messages", async (req, res) => {
+  // Route received a request to adapter for processing
+  await adapter.process(req, res, (context) => bot.run(context));
 });
